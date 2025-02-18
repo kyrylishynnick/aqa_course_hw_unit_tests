24f5c804bb128e7c0b2dc6100484b32706f39439
nikolaykyrylishyn <nikolay.kyrylishyn@gmail.com>
2025-02-18 13:58:54 +0200

    home work 4

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,20 @@
   преобразовываясь в number
 
 */
+
+
+let minAge = 18;
+let maxAge = 60;
+let age;
+
+if (isNaN(age)) {
+  console.log ('Incorrect data type');
+} else if (age < minAge) {
+  console.log (`You don't have access cause your age is " ${age}. " It's less then " ${minAge}`);
+} else if (age >= minAge && age <= maxAge){
+  console.log ("Welcome !");
+} else if (age > maxAge) {
+  console.log ("Keep calm and look Culture channel");
+} else {
+  console.log ("Technical work");
+}
