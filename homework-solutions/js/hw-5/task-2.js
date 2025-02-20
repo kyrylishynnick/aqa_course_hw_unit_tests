dd99e6cb9fc91bec14f901607ef377a5b57fc519
nikolaykyrylishyn <nikolay.kyrylishyn@gmail.com>
2025-02-20 15:50:56 +0200

    add home work 5

--- homework-solutions/js/hw-5/task-2.js
+++ homework-solutions/js/hw-5/task-2.js
@@ -18,4 +18,22 @@
  */
 let fizzBuzzResult = '';
 
+for (let i = 1; i <= 100; i++) {
+    fizzBuzzResult += i;
+    
+    if (i % 3 === 0 && i % 5 === 0) {
+        fizzBuzzResult += ` - делится и на 3 и на 5`;
+    }
+    else if (i % 3 === 0) {
+        fizzBuzzResult += ` - делится на 3`;
+    }
+    else if (i % 5 === 0) {
+        fizzBuzzResult += ` - делится на 5`;
+    }
+   fizzBuzzResult += '\n';
+}
+
+fizzBuzzResult = fizzBuzzResult.trim();
+console.log (fizzBuzzResult)
+
 export { fizzBuzzResult };
